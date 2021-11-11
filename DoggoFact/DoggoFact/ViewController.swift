03cac9f87b685ad8628b7794c66685f58e4f3894
Букшев Иван Евгеньевич <ibukshev@alfabank.ru>
2021-11-11 13:49:10 +0700

    Init commit with empty project

---
+++ DoggoFact/DoggoFact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoggoFact
+//
+//  Created by Иван Букшев on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
